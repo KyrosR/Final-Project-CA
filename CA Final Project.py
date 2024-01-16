ccc6db1fdf50ca5d644ceb246fc027de95352ab8
rubitubie <ruben.aaltink@gmail.com>
2024-01-16 12:53:31 +0100

    update les 1

--- CA Final Project.py
+++ CA Final Project.py
@@ -1,2 +1,22 @@
 #Cellular Automata
-#heyyyyy
+#Class for 1 dim CA
+class OnedimCA:
+    def __init__(self, cell_amount, start_pattern, apply_rule, layers_amount):
+        self.cell_amount = cell_amount
+        self.start_patern = start_pattern
+        self.apply_rule = apply_rule
+        self.layers_amount = layers_amount
+        self.cells = []
+
+    def cells(self):
+        self.cells = list(self.start_patern)
+
+
+    def rule_30(self):
+        print("moet nog")
+
+    def newgeneration(self):
+        print("moet nog")
+
+        
+
