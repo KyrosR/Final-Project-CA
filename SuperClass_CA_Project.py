4381b6f11ae793399cc66b2791d56bb42b8acf42
KyrosR <k.m.renooij@gmail.com>
2024-01-31 16:45:54 +0100

    benamingen aangepast. bord is nu board, colom is column en patern is pattern.

--- SuperClass_CA_Project.py
+++ SuperClass_CA_Project.py
@@ -7,16 +7,16 @@ Access to this super class is granted by importing it.
 """""""""
 
 class CA:
-    # The __init__ function contains all variabels for both CA"s.
+    # The __init__ function contains all variables for both CAs.
 
     def __init__(self, start_pattern, apply_rule, layers_amount, boundary_con):
-        # The pattern of one's and zero's the 1 dimensional array
-        self.start_patern = start_pattern
-        # The rule that is applied to a given array
+        # The pattern of ones and zeros the one-dimensional array.
+        self.start_pattern = start_pattern
+        # The rule that is applied to a given array.
         self.apply_rule = apply_rule  
-        # The amount of array's(layers) of nextgeneration cells.
+        # The amount of arrays (layers) of next generation cells.
         self.layers_amount = layers_amount 
-        # Putting the start_pattern in a list
-        self.cells = list(self.start_patern)
-        # The boundary conditions for the firs/last cell of the array.
+        # Putting the start_pattern in a list.
+        self.cells = list(self.start_pattern)
+        # The boundary conditions for the first/last cell of the array.
         self.boundary_con = boundary_con
--- TwoD_CA_Project.py
+++ TwoD_CA_Project.py
@@ -25,75 +25,75 @@ class TwodimCA(CA):
     in the values they want for those variables.
     """""""""
 
-    def __init__(self, start_pattern, apply_rule, layers_amount, boundary_con, row, colom):
+    def __init__(self, start_pattern, apply_rule, layers_amount, boundary_con, row, column):
         # Uses variables written in the superclass
         super().__init__(start_pattern, apply_rule, layers_amount, boundary_con)
-        # The amount of rows and coloms.
+        # The amount of rows and columns.
         self.row = row
-        self.colom = colom
+        self.column = column
         
         # Necessary functions and variables for the plot of the the animation.
         self.fig, self.ax = plt.subplots()
         plt.axis("off")
-        self.im = self.ax.imshow(self.start_patern, cmap='magma', interpolation='nearest')
+        self.im = self.ax.imshow(self.start_pattern, cmap='magma', interpolation='nearest')
         self.ani = animation.FuncAnimation(self.fig, self.update, frames=100, interval=50, save_count=50, blit=True)
 
         
     """""""""
-    This function makes a copy of the bord and assigns it to self.cells.
+    This function makes a copy of the board and assigns it to self.cells.
     """""""""
 
-    def new(self,bord):
+    def new(self,board):
         list = []
         for q in range(1, self.row+1):
             list.append([])
-            for w in range(1, self.colom+1):
-                list[q-1].append(bord[q-1][w-1])
+            for w in range(1, self.column+1):
+                list[q-1].append(board[q-1][w-1])
         self.cells =  list
 
 
 
 
     """""""""
-    This function updates the new_bord and then assigns the new_bord to the original bord (self.startpatern).
+    This function updates the new_board and then assigns the new_board to the original board (self.start_pattern).
     This function uses periodic boundaries.
     """""""""
 
     def update(self,frame):
-        # Makes a copy of the old bord.
-        self.new(self.start_patern) 
-        nieuw_bord = self.cells
+        # Makes a copy of the old board.
+        self.new(self.start_pattern) 
+        nieuw_board = self.cells
 
-        # Determines the new state of each cell from the old bord by checking all its neighbors.
-        # Then it updates each cell in the new_bord.
+        # Determines the new state of each cell from the old board by checking all its neighbors.
+        # Then it updates each cell in the new_board.
         for i in range(1,self.row+1):
-            for j in range(1, self.colom+1):
+            for j in range(1, self.column+1):
 
                 # Checks if the cell is alive, else the cell is dead.
-                if self.start_patern[i-1][j-1] == 1:
+                if self.start_pattern[i-1][j-1] == 1:
 
                     # The sum of all neighbors equals the amount of alive cells.
-                    sum = (self.start_patern[i-2][j-2] + self.start_patern[i-2][j-1] + self.start_patern[i-2][j%self.colom] +
-                    self.start_patern[i-1][j-2] + self.start_patern[i-1][j%self.colom] +
-                    self.start_patern[i%self.row][j-2] + self.start_patern[i%self.row][j-1] + self.start_patern[i%self.row][j%self.colom])
+                    sum = (self.start_pattern[i-2][j-2] + self.start_pattern[i-2][j-1] + self.start_pattern[i-2][j%self.column] +
+                    self.start_pattern[i-1][j-2] + self.start_pattern[i-1][j%self.column] +
+                    self.start_pattern[i%self.row][j-2] + self.start_pattern[i%self.row][j-1] + self.start_pattern[i%self.row][j%self.column])
                     
-                    # Updates the cell in the new_bord.
+                    # Updates the cell in the new_board.
                     if sum < 2 or sum > 3:
-                        nieuw_bord[i-1][j-1] = 0
+                        nieuw_board[i-1][j-1] = 0
 
                 else:
                     # The sum of all neighbors equals the amount of alive cells.
-                    sum = (self.start_patern[i-2][j-2] + self.start_patern[i-2][j-1] + self.start_patern[i-2][j%self.colom] +
-                    self.start_patern[i-1][j-2] + self.start_patern[i-1][j%self.colom] +
-                    self.start_patern[i%self.row][j-2] + self.start_patern[i%self.row][j-1] + self.start_patern[i%self.row][j%self.colom])
-                    # Updates the cell in the new_bord.
+                    sum = (self.start_pattern[i-2][j-2] + self.start_pattern[i-2][j-1] + self.start_pattern[i-2][j%self.column] +
+                    self.start_pattern[i-1][j-2] + self.start_pattern[i-1][j%self.column] +
+                    self.start_pattern[i%self.row][j-2] + self.start_pattern[i%self.row][j-1] + self.start_pattern[i%self.row][j%self.column])
+                    # Updates the cell in the new_board.
                     if sum == 3:
-                        nieuw_bord[i-1][j-1] = 1
+                        nieuw_board[i-1][j-1] = 1
 
-        # The old bord becomes the new_bord.                
-        self.start_patern = nieuw_bord
-        # Makes an image of the new_bord.
-        self.im.set_data(self.start_patern)
+        # The old board becomes the new_board.                
+        self.start_pattern = nieuw_board
+        # Makes an image of the new_board.
+        self.im.set_data(self.start_pattern)
         # Returns that image to the animation.
         return self.im,
         
@@ -108,7 +108,7 @@ class TwodimCA(CA):
 # Makes k an instance of the class with the values of the variables that are necessary for the program to run.
 # Could be changed to accept inputs for a more interactive style.
 k = TwodimCA([[0,0,1,0,0,0],[1,0,1,0,0,0],[0,1,1,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]], "Game of life", 25, "periodic", 5, 6)
-# Shows all the new_bords in the animation. 
+# Shows all the new_boards in the animation. 
 plt.show()
 
 
